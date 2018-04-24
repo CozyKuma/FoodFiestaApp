57e9f4bcf5dd2329fb41abd3bc34e13e73f9030b
CozyKuma <nedvar17@student.aau.dk>
2018-04-24 19:37:36 +0200

    Calendar dialog implementet

--- FoodInventory/app/src/main/java/com/example/cozykuma/foodinventory/AddItemActivity.java
+++ FoodInventory/app/src/main/java/com/example/cozykuma/foodinventory/AddItemActivity.java
@@ -1,15 +1,20 @@
 package com.example.cozykuma.foodinventory;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-import java.util.Date;
+import java.util.Calendar;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -17,9 +22,10 @@ public class AddItemActivity extends AppCompatActivity {
     private FoodCategory category;
     private String expireDate;
     private EditText mEditTextName;
-    private EditText mEditTextDate;
+    private TextView mDateView;
     private Spinner mSpinner;
     private Button mButton;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
 
 
     @Override
@@ -28,7 +34,7 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
 
         mEditTextName = (EditText) findViewById(R.id.textName);
-        mEditTextDate = (EditText) findViewById(R.id.textDate);
+        mDateView = (TextView) findViewById(R.id.dateTextView);
         mSpinner = (Spinner) findViewById(R.id.categorySpinner);
         mButton = (Button) findViewById(R.id.addItemBtn);
 
@@ -39,16 +45,45 @@ public class AddItemActivity extends AppCompatActivity {
             }
         });
 
+        // Category Field + Load Content
         ArrayAdapter<FoodCategory> adapter = new ArrayAdapter<FoodCategory>(getApplicationContext(), android.R.layout.simple_spinner_item, FoodCategory.getCategoryList());
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         mSpinner.setAdapter(adapter);
+
+        // Calendar onClickListener
+        mDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddItemActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+                }
+            }
+        );
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month++;
+                String date = dayOfMonth + "-" + month + "-" + year;
+                mDateView.setText(date);
+            }
+        };
     }
 
     public void AddItem() {
         itemName = mEditTextName.getText().toString();
         category = (FoodCategory) mSpinner.getSelectedItem();
-        expireDate = mEditTextDate.getText().toString();
+        expireDate = mDateView.getText().toString();
 
         FoodItem newItem = new FoodItem(itemName, expireDate);
 
