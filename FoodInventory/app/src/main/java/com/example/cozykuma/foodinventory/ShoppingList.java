918d6b859c549e1a5717bd18392ea7a2b7e0d924
CozyKuma <nedvar17@student.aau.dk>
2018-05-11 11:34:31 +0200

    Portrait mode on all activities

--- FoodInventory/app/src/main/java/com/example/cozykuma/foodinventory/ShoppingList.java
+++ FoodInventory/app/src/main/java/com/example/cozykuma/foodinventory/ShoppingList.java
@@ -136,6 +136,7 @@ public class ShoppingList extends AppCompatActivity{
     @Override
     protected void onResume() {
         super.onResume();
+        adapter.notifyDataSetChanged();
     }
 
     @Override
